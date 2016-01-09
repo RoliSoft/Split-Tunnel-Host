2838ac5792e110a081b419f8facec98e5f75fcb2
RoliSoft <rolisoft@gmail.com>
2016-01-09 23:17:40 +0200

    Added whois lookup for proper CIDR resolve.

--- gen_addrs.sh
+++ gen_addrs.sh
@@ -12,7 +12,7 @@ while read dns; do
 	dig +noall +answer aaaa "$dns" | awk '$4~/^AAAA$/{print $5}' >> addrs_v6.txt
 done <domains.txt
 
-# remove duplicates since Windows will complain otherwise
+# remove duplicates before lookup
 
 echo Deduplicating list...
 
@@ -20,14 +20,31 @@ echo Deduplicating list...
 { rm addrs_v6.txt && sort -n | uniq > addrs_v6.txt; } < addrs_v6.txt
 
 # transfrom address list into a CIDR list
-# this is problematic, normally you would do a whois for the IP and use the returned range,
-# but in most cases this would return a CIDR which covers all the servers in use by the
-# cloud provider, and this might not be what we want
-# the current implementation just uses /24 for IPv4 and /64 for IPv6
 
-echo Processing list...
+echo Resolving addresses...
 
-{ rm addrs_v4.txt && awk -F'.' '{print $1"."$2"."$3".0/24"}' > addrs_v4.txt; } < addrs_v4.txt
-{ rm addrs_v6.txt && awk -F'.' '{print $0"/64"}' > addrs_v6.txt; } < addrs_v6.txt
+echo -n > addrs_v4_tmp.txt
+echo -n > addrs_v6_tmp.txt
+
+while read addr; do
+	whois "$addr" | awk '/^(CIDR|route):/&&/\//{print $2}' | tr -d ', ' >> addrs_v4_tmp.txt
+done <addrs_v4.txt
+
+while read addr; do
+	whois "$addr" | awk '/^inet6num:/&&/\//{print $2}' | tr -d ', ' >> addrs_v6_tmp.txt
+done <addrs_v6.txt
+
+cat addrs_v4_tmp.txt > addrs_v4.txt
+cat addrs_v6_tmp.txt > addrs_v6.txt
+
+rm -f addrs_v4_tmp.txt
+rm -f addrs_v6_tmp.txt
 
 echo "94.125.179.8/32" >> addrs_v4.txt
+
+# remove duplicates since Windows will complain otherwise
+
+echo Deduplicating list...
+
+{ rm addrs_v4.txt && sort -n | uniq > addrs_v4.txt; } < addrs_v4.txt
+{ rm addrs_v6.txt && sort -n | uniq > addrs_v6.txt; } < addrs_v6.txt
