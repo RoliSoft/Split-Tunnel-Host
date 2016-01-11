8bd3324fbd7873d6b950fc1d2b5c17001de1e7cb
RoliSoft <rolisoft@gmail.com>
2016-01-11 04:35:52 +0200

    Implemented routing.

--- dnsserv.go
+++ dnsserv.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"strings"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -16,6 +17,8 @@ import (
 var nameservers []string
 var verbose *bool
 var gateway *string
+var router string
+var routed []string
 
 func getErrMsg(w dns.ResponseWriter, req *dns.Msg, err int) *dns.Msg {
 	m := new(dns.Msg)
@@ -55,15 +58,22 @@ func getNsReply(w dns.ResponseWriter, req *dns.Msg) *dns.Msg {
 func handleRequest(w dns.ResponseWriter, req *dns.Msg) {
 	var m *dns.Msg
 
-	if len(req.Question) > 0 && strings.HasSuffix(req.Question[0].Name, ".netflix.com.") {
+	if len(req.Question) > 0 && (req.Question[0].Name == "netflix.com." || strings.HasSuffix(req.Question[0].Name, ".netflix.com.")) {
 		if req.Question[0].Qtype == dns.TypeA {
 			m = getNsReply(w, req)
 			for _, ans := range m.Answer {
 				if ans.Header().Rrtype == dns.TypeA {
 					ip := ans.(*dns.A).A.String()
+					routed = append(routed, ip)
+
 					log.Print("Re-routing ", ip, " for ", ans.Header().Name, "/", dns.Type(ans.Header().Rrtype).String())
-					// TODO reroute
-					//"route add " + ip + "/32 " + *gateway
+
+					out, err := exec.Command(router, "add", ip + "/32", *gateway).CombinedOutput()
+					if err != nil {
+						log.Print(err)
+					} else if *verbose {
+						log.Printf("route: %s", out)
+					}
 				} else if ans.Header().Rrtype == dns.TypeAAAA {
 					// sanity check for now, shouldn't happen afaik
 					log.Print("WARNING: AAAA response in ", ans.Header().Name, "/A")
@@ -85,13 +95,22 @@ func handleRequest(w dns.ResponseWriter, req *dns.Msg) {
 }
 
 func main() {
-	gateway = flag.String("gw", "192.168.1.1", "gateway IP to re-route to")
+	gateway = flag.String("r", "", "gateway IP for routing destination")
 	verbose = flag.Bool("v", false, "verbose logging")
 
 	flag.Usage = func() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
+
+	router, _ = exec.LookPath("route")
+	if len(router) < 1 {
+		log.Fatal("Unable to find the `route` command in your %PATH%.")
+	}
+
+	if len(*gateway) < 7 {
+		log.Fatal("Gateway IP must be specified via argument `r`.")
+	}
 
 	log.Print("Starting DNS resolver...")
 
@@ -122,6 +141,19 @@ func main() {
 	for {
 		select {
 		case s := <-sig:
+			if len(routed) > 0 {
+				log.Print("Removing routes...")
+
+				for _, ip := range routed {
+					out, err := exec.Command(router, "delete", ip + "/32").CombinedOutput()
+					if err != nil {
+						log.Print(err)
+					} else if *verbose {
+						log.Printf("route: %s", out)
+					}
+				}
+			}
+
 			log.Fatalf("Received signal %d, exiting...", s)
 		}
 	}
